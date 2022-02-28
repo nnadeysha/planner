50f7854b7951794124194c8c3dd2114e49aacbed
novikova.n_qschool <nadeysha23@gmail.com>
2022-03-01 02:04:25 +0400

    feat: add metadata

--- src/components/backlog.js
+++ src/components/backlog.js
@@ -15,8 +15,8 @@ export const backlogContent = async (arrUsers) => {
   dragAndDrop(arrTask);
   document.querySelectorAll('.header__button').forEach(button =>{
     button.addEventListener('click', ()=>{
-      dragAndDrop(arrTask);
       taskInCellCreate(arrTask, arrUsers);
+      dragAndDrop(arrTask);
     })
 })
 };
@@ -33,32 +33,36 @@ function backlogCreate(arrTask){
   })
   
 }
-function taskInCellCreate(arrTask, arrUsers, itemsLocalSt){
+function taskInCellCreate(arrTask, arrUsers){
+  
   const cells = document.querySelectorAll(".cell");
   arrTask.map((task) =>{
-    for (let i = 0; i < arrUsers.length; i++) {
-      if (arrUsers[i].id === task.executor) {
-        let dateData = task.planStartDate.replace(/-/gi, ".").slice(5, 10);
-        let coverDate = dateData.slice(3, 5) + "." + dateData.slice(0, 2);
-        cells.forEach((el) => {
-          if (
-            el.getAttribute("data-date") === coverDate &&
-            +el.getAttribute("data-count") === arrUsers[i].id
-          ) {
-            const cellContent = document.createElement("div");
-            cellContent.setAttribute("draggable", "true");
-            cellContent.setAttribute("data-id", `${task.id}`);
-            cellContent.classList.add("task__element");
-            el.appendChild(cellContent);
-  
-            cellContent.innerHTML += `<p class="task__description"   data-tooltip="Start:${task.planStartDate} Deadline:${task.planEndDate}">${task.subject}</p>`;
+    if (task.executor !== null){
+      for (let i = 0; i < arrUsers.length; i++) {
+        if (arrUsers[i].id === task.executor) {
+          let dateData = task.planStartDate.replace(/-/gi, ".").slice(5, 10);
+          let coverDate = dateData.slice(3, 5) + "." + dateData.slice(0, 2);
+          cells.forEach((el) => {
+            if (
+              el.getAttribute("data-date") === coverDate &&
+              +el.getAttribute("data-count") === arrUsers[i].id
+            ) {
+              const cellContent = document.createElement("div");
+              cellContent.setAttribute("draggable", "true");
+              cellContent.setAttribute("data-id", `${task.id}`);
+              cellContent.classList.add("task__element");
+              el.appendChild(cellContent);
+    
+              cellContent.innerHTML += `<p class="task__description"   data-tooltip="Start:${task.planStartDate} Deadline:${task.planEndDate}">${task.subject}</p>`;
+              
+            }
+    
             
-          }
-  
-          
-        });
+          });
+        }
       }
     }
+    
   })
   
 }
